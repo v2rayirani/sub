9837c185f3bddb5a0b1923650445cc965530201d
v2rayirani <v2rayirani1@gmail.com>
2024-06-05 14:25:16 +0330

    Update v2ray irani.py

--- v2ray irani.py
+++ v2ray irani.py
@@ -4,10 +4,10 @@ app = Flask(__name__)
 
 @app.route('/generate_subscription', methods=['POST'])
 def generate_subscription():
-    # لینک سابسکریپشن به عنوان الگو
+    # لینک سابسکریپشن واقعی شما
     subscription_url = "https://github.com/v2rayirani/sublink/blob/main/sub.text"
 
-    # دریافت مقادیر از لینک سابسکریپشن
+    # دریافت مقادیر از درخواست JSON
     uuid = request.json.get('uuid')
     domain = request.json.get('domain')
     path = request.json.get('path')
@@ -15,7 +15,23 @@ def generate_subscription():
     # جایگزینی مقادیر UUID، domain و path در لینک سابسکریپشن
     subscription_link = subscription_url.replace('domain', domain).replace('uuid', uuid).replace('path', path)
 
-    return jsonify({"subscription_link": subscription_link})
+    # استخراج کانفیگ‌ها از لینک سابسکریپشن
+    config_params = subscription_link.split("?")[-1].split("&")
+    config = {}
+    for param in config_params:
+        key, value = param.split("=")
+        config[key] = value
+
+    # جایگزینی مقادیر کانفیگ با مقادیر ورودی کاربر
+    for key in config.keys():
+        if key != 'uuid' and key != 'domain' and key != 'path':
+            config[key] = request.json.get(key)
+
+    # ایجاد لینک نهایی با مقادیر جدید
+    final_subscription_url = subscription_link.split("?")[0] + "?"
+    final_subscription_url += "&".join([f"{key}={value}" for key, value in config.items()])
+
+    return jsonify({"subscription_link": final_subscription_url})
 
 if __name__ == '__main__':
     app.run(debug=True)
