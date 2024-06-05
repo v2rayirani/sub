473bfbc969303f1750edcbe10935f2b5c4499b9e
v2rayirani <v2rayirani1@gmail.com>
2024-06-05 14:02:37 +0330

    Add files via upload

---
+++ v2ray irani.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/generate_subscription', methods=['POST'])
+def generate_subscription():
+    # لینک سابسکریپشن به عنوان الگو
+    subscription_url = "https://my.link.io/sub?host=domain=uuid=/path"
+
+    # دریافت مقادیر از لینک سابسکریپشن
+    uuid = request.json.get('uuid')
+    domain = request.json.get('domain')
+    path = request.json.get('path')
+
+    # جایگزینی مقادیر UUID، domain و path در لینک سابسکریپشن
+    subscription_link = subscription_url.replace('domain', domain).replace('uuid', uuid).replace('path', path)
+
+    return jsonify({"subscription_link": subscription_link})
+
+if __name__ == '__main__':
+    app.run(debug=True)
